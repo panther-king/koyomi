5525e9f655ae9a11a63f9fa4a807c224af0b5442
Taro Yamashita <taro.ymst@gmail.com>
2018-05-11 08:29:36 +0900

    ビルダーを利用してカレンダーオブジェクトを生成できるようにした。

--- src/calendar.rs
+++ src/calendar.rs
@@ -1,5 +1,8 @@
 //! # カレンダー
-pub fn num_days(year: i32, month: u32) -> u8 {
+use KoyomiError;
+use date::Date;
+
+pub fn num_days(year: i32, month: u32) -> u32 {
     match month {
         1 | 3 | 5 | 7 | 8 | 10 | 12 => 31,
         4 | 6 | 9 | 11 => 30,
@@ -20,6 +23,118 @@ pub fn is_leap(year: i32) -> bool {
         false
     } else {
         true
+    }
+}
+
+#[derive(Debug)]
+pub struct Calendar {
+    from: Date,
+    until: Date,
+}
+
+impl Calendar {
+    pub fn new(from: Date, until: Date) -> Result<Self, KoyomiError> {
+        if until.num_days(&from) <= 0 {
+            Err(KoyomiError::InvalidTerm(from, until))
+        } else {
+            Ok(Calendar { from, until })
+        }
+    }
+
+    pub fn build<'a>() -> CalendarBuilder<'a> {
+        CalendarBuilder {
+            from: None,
+            single: None,
+            until: None,
+        }
+    }
+
+    pub fn from(&self) -> String {
+        self.from.to_string()
+    }
+
+    pub fn until(&self) -> String {
+        self.until.to_string()
+    }
+}
+
+#[derive(Debug)]
+pub struct CalendarBuilder<'a> {
+    from: Option<&'a str>,
+    single: Option<&'a str>,
+    until: Option<&'a str>,
+}
+
+impl<'a> CalendarBuilder<'a> {
+    pub fn finalize(&self) -> Result<Calendar, KoyomiError> {
+        if let Some(single) = self.single {
+            self.single_calendar(single)
+        } else {
+            let from = self.date_from()?;
+            let until = self.date_until()?;
+            Calendar::new(from, until)
+        }
+    }
+
+    pub fn from(mut self, from: &'a str) -> Self {
+        self.from = Some(from);
+        self
+    }
+
+    pub fn single(mut self, single: &'a str) -> Self {
+        self.single = Some(single);
+        self
+    }
+
+    pub fn until(mut self, until: &'a str) -> Self {
+        self.until = Some(until);
+        self
+    }
+
+    fn date_from(&self) -> Result<Date, KoyomiError> {
+        match self.from {
+            None => Err(KoyomiError::NotEnough),
+            Some(f) => match f.split("-").count() {
+                1 => Date::parse(&format!("{}-01-01", f)),
+                2 => Date::parse(&format!("{}-01", f)),
+                _ => Err(KoyomiError::InvalidFormat(f.into())),
+            },
+        }
+    }
+
+    fn date_until(&self) -> Result<Date, KoyomiError> {
+        match self.until {
+            None => Err(KoyomiError::NotEnough),
+            Some(u) => match u.split("-").count() {
+                1 => Date::parse(&format!("{}-12-31", u)),
+                2 => self.end_of_month(&format!("{}-01", u)),
+                _ => Err(KoyomiError::InvalidFormat(u.into())),
+            },
+        }
+    }
+
+    fn end_of_month(&self, fmt: &str) -> Result<Date, KoyomiError> {
+        let first = Date::parse(fmt)?;
+        let (y, m) = (first.year(), first.month());
+        Date::from_ymd(y, m, num_days(y, m))
+    }
+
+    fn single_calendar(&self, ym: &str) -> Result<Calendar, KoyomiError> {
+        let splits = ym.split("-").collect::<Vec<_>>();
+        match splits.len() {
+            1 => {
+                let from = Date::parse(&format!("{}-01-01", ym))?;
+                let until = Date::parse(&format!("{}-12-31", ym))?;
+                Ok(Calendar { from, until })
+            }
+            2 => {
+                let fmt = format!("{}-{}-01", splits[0], splits[1]);
+                let from = Date::parse(&fmt)?;
+                let until = self.end_of_month(&fmt)?;
+                Ok(Calendar { from, until })
+            }
+            _ => Err(KoyomiError::InvalidFormat(ym.into())),
+        }
     }
 }
 
@@ -64,5 +179,71 @@ mod tests {
         assert!(!is_leap(2017));
         // うるう年ではない(100で割り切れるが400で割り切れない)
         assert!(!is_leap(2100));
+    }
+
+    #[test]
+    fn valid_calendar() {
+        let from = Date::parse("2018-04-01").unwrap();
+        let until = Date::parse("2018-04-30").unwrap();
+        assert!(Calendar::new(from, until).is_ok());
+    }
+
+    #[test]
+    fn invalid_calendar() {
+        let from = Date::parse("2018-04-01").unwrap();
+        let until = Date::parse("2017-04-01").unwrap();
+        assert!(Calendar::new(from, until).is_err());
+    }
+
+    #[test]
+    fn calendar_from() {
+        let from = Date::parse("2018-04-01").unwrap();
+        let until = Date::parse("2018-04-30").unwrap();
+        let calendar = Calendar::new(from, until).unwrap();
+        assert_eq!(calendar.from(), "2018-04-01");
+    }
+
+    #[test]
+    fn calendar_until() {
+        let from = Date::parse("2018-04-01").unwrap();
+        let until = Date::parse("2018-04-30").unwrap();
+        let calendar = Calendar::new(from, until).unwrap();
+        assert_eq!(calendar.until(), "2018-04-30");
+    }
+
+    #[test]
+    fn builder_single() {
+        let c = Calendar::build().single("2018").finalize();
+        assert!(c.is_ok());
+
+        let c = Calendar::build().single("2018-04").finalize();
+        assert!(c.is_ok());
+
+        let c = Calendar::build().single("abc").finalize();
+        assert!(c.is_err());
+    }
+
+    #[test]
+    fn builder_between() {
+        let c = Calendar::build().from("2017").until("2018").finalize();
+        assert!(c.is_ok());
+
+        let c = Calendar::build()
+            .from("2017-04")
+            .until("2018-04")
+            .finalize();
+        assert!(c.is_ok());
+
+        let c = Calendar::build().from("2017").until("2018-04").finalize();
+        assert!(c.is_ok());
+
+        let c = Calendar::build().from("2017-04").until("2018").finalize();
+        assert!(c.is_ok());
+
+        let c = Calendar::build().from("abc").until("2018").finalize();
+        assert!(c.is_err());
+
+        let c = Calendar::build().from("2017").until("abc").finalize();
+        assert!(c.is_err());
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -5,9 +5,13 @@ mod date;
 mod era;
 mod holiday;
 
+pub use date::Date;
+
 #[derive(Debug)]
 pub enum KoyomiError {
     InvalidFormat(String),
+    InvalidTerm(Date, Date),
+    NotEnough,
     NoTomorrow(i32, u32, u32),
     NoYesterday(i32, u32, u32),
 }
